8bfc82d9e4fcb6b682a2764a8399d2b24a04be62
omkarj88 <59362849+omkarj88@users.noreply.github.com>
2019-12-30 18:11:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App'
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
