abb8c42ba6876b0e83b727ebaa15ded7b0cefbd7
omkarj88 <59362849+omkarj88@users.noreply.github.com>
2019-12-30 18:09:55 +0530

    Update Roster.js

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -5,8 +5,6 @@ import Player from './Player'
 import Resume from './Resume'
 import Schedule from './Schedule' 
 
-// The Roster component matches one of two different routes
-// depending on the full pathname
 const Roster = () => (
   <Switch>
     <Route exact path='/roster' component={FullRoster}/>
