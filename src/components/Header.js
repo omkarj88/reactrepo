19ceed42e821bd27a12531b248a3ba81c4e86105
omkarj88 <59362849+omkarj88@users.noreply.github.com>
2019-12-30 18:08:11 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// The Header creates links that can be used to navigate
-// between routes.
 const Header = () => (
   <header>
-    {/* <nav> */}
       <ul className="nav nav-pills">
         <li className=''><Link to='/'>Login</Link></li>
         <li className=''><Link to='/roster'>Employee List</Link></li>
-        {/* <li className=''><Link to='/schedule'>Schedule</Link></li> */}
-        {/* <li className=''><Link to='/schedule'>Show Employee </Link></li> */}
       </ul>
-    {/* </nav> */}
   </header>
 )
 
